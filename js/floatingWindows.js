5b9711f290e09946926e4f7b67361c51aa81dd45
Daniel Martí <49535803+damacaa@users.noreply.github.com>
2023-02-24 14:52:49 +0100

    Added some cool animations

--- js/floatingWindows.js
+++ js/floatingWindows.js
@@ -9,10 +9,27 @@
     }
 })*/
 
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function () {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+
+function enableScroll() {
+    window.onscroll = function () { };
+}
+
 
 function ShowFloatingDiv(name) {
     var gameInfo = document.getElementById("projectPageWindow");
     gameInfo.style.display = 'block';
+    if (gameInfo.classList.contains('closeWindowAnimation'))
+        gameInfo.classList.remove('closeWindowAnimation');
 
     var iframe = document.getElementById("projectPageiFrame");
     if (name.includes(".")) {
@@ -21,12 +38,23 @@ function ShowFloatingDiv(name) {
         iframe.src = "/projects/" + name + ".html";
 
     }
+    disableScroll();
 }
 
 function CloseFloatingDiv() {
     var gameInfo = document.getElementById("projectPageWindow");
-    gameInfo.style.display = 'none';
 
-    var iframe = document.getElementById("projectPageiFrame");
-    iframe.src = "";
+    //gameInfo.style.display = 'none';
+    gameInfo.classList.add('closeWindowAnimation');
+    enableScroll();
+
+    const myTimeout = setTimeout(myGreeting, 800);
+
+    function myGreeting() {
+        gameInfo.style.display = 'none';
+
+        var iframe = document.getElementById("projectPageiFrame");
+        iframe.src = "";
+
+    }
 }
